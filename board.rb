bce0ceeeb40ccbd2f6f67dfa3f894680a02e955a
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-07-22 20:41:35 -0700

    Added method to generate top string of gui

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 class Board
+  
   def initialize
     @grid = (0...9).map { |row| [nil] * 9 }
     @bomb_positions = bomb_positions
@@ -11,6 +12,12 @@ class Board
 
   def []=(pos, tile)
     @grid[pos.first][pos.last] = tile
+  end
+  
+  def add_bomb_count
+    all_positions.each do |pos|
+      self[pos].calculate_bomb_count
+    end
   end
 
   def all_positions
@@ -32,20 +39,21 @@ class Board
   def on_board?(pos)
     (0...9).include?(pos.first) && (0...9).include?(pos.last)
   end
+  
+  def place_tile(pos)
+    if @bomb_positions.include?(pos)
+      self[pos] = Tile.new(pos, self, true)
+    else
+      self[pos] = Tile.new(pos, self, false)
+    end
+  end
 
-  # Refactor
   def populate_grid
     all_positions.each do |pos|
-      if @bomb_positions.include?(pos)
-        self[pos] = Tile.new(pos, self, true)
-      else
-        self[pos] = Tile.new(pos, self, false)
-      end
+      place_tile(pos)
     end
-
-    all_positions.each do |pos|
-      self[pos].calculate_bomb_count
-    end
+    
+    add_bomb_count
   end
 
   def reveal_all
@@ -54,7 +62,6 @@ class Board
     end
   end
 
-  # Where should this be?
   def reveal_neighbors(pos)
     unless self[pos].bomb_count == 0
       self[pos].reveal
@@ -66,24 +73,37 @@ class Board
       reveal_neighbors(neighbor.position)
     end
   end
-
-  # this is bad form, p in to_s method
-  def to_s
-    new_board = @grid.dup.map do |row|
+  
+  def stringify_tiles
+    @grid.dup.map do |row|
        row.map do |elem|
         if elem.revealed?
           elem.to_s
         elsif elem.flagged?
           "F"
         else
-           "#"
+          "█▌"
         end
       end
     end
+  end
 
-    new_board.each do |row|
+  # this is bad form, p in to_s method
+  def to_s
+    stringified_board = self.stringify_tiles
+
+    stringified_board.each do |row|
       p row
     end
+  end
+  
+  def top_row_string
+    top_row = ""
+    (0...9).each do |i|
+      top_row += " " + i.to_s 
+    end
+    
+    top_row + "\n " + "-" * 17
   end
 
   def won?
